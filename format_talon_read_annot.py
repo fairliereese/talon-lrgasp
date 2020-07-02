ab1ac83f3f02a8ab3d9cd3fb23166d952a8ad798
fairliereese <fairliek@comcast.net>
2020-07-01 17:05:46 -0700

    added sample names option

--- format_talon_read_annot.py
+++ format_talon_read_annot.py
@@ -7,16 +7,29 @@ def get_args():
 		help='TALON read annot file to reformat')
 	parser.add_argument('-o', dest='oprefix',
 		help='output file prefix')
+	parser.add_argument('-samples', dest='samples',
+		default=None,
+		help='comma-separated list of sample names to include. '
+		'If not provided, includes all samples.')
 	args = parser.parse_args()
 	return args
 
 def main():
 	args = get_args()
 
-	df = pd.read_csv(args.annotfile, sep='\t', usecols=[0,10])
+	if args.samples:
+		sample_names = args.samples.split(',')
+		print(sample_names)
+
+	df = pd.read_csv(args.annotfile, sep='\t', usecols=[0,1,10])
 	df.rename({'read_name':'read_id','transcript_ID':'transcript_id'},
 		axis=1, 
 		inplace=True)
+
+	if args.samples:
+		df = df.loc[df.dataset.isin(sample_names)]
+
+	df.drop('dataset', axis=1, inplace=True)
 
 	ofile = args.oprefix+'_lrgasp_read_annot.tsv'
 	df.to_csv(ofile, sep='\t', index=False)
